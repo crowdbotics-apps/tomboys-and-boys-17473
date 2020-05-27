7235622365cafd6713eaa2246f54c76062d41919
Crowdbotics <team@crowdbotics.com>
2020-05-27 22:53:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tomboys_and_boys_17473/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tomboys_and_boys_17473;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "tomboys_and_boys_17473";
+    }
+}
